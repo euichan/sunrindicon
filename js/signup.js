7e335d5dc2228245298075896c811fab48b4c1ca
Seohyung <dkstjgud98@naver.com>
2014-10-15 09:06:44 +0900

    10월 15

--- js/signup.js
+++ js/signup.js
@@ -1,5 +1,14 @@
 function signup_next1(event)
 {
-    document.getElementById("signup_box1").style.left="-100%";
-    document.getElementById("signup_box2").style.left="0%";
+    document.getElementById("signup_box").style.left="-100%";
+}
+
+function signup_next2(event)
+{
+    document.getElementById("signup_box").style.left="-200%";
+}
+
+function signup_next3(event)
+{
+    document.getElementById("signup_box").style.left="-300%";
 }
